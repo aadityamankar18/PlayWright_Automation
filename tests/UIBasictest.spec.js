a3b37f17aac43ab4c7f3740ab4d9308b45d32b98
aaditya mankar <aadityamankar18@gmail.com>
2026-06-24 10:22:30 +0530

    initial changes

---
+++ tests/UIBasictest.spec.js
@@ -0,0 +1,18 @@
+const {test, expect} = require("@playwright/test");
+
+test('First Playwright test', async ({browser})=>
+{ 
+    //playwright code -
+    const context =  await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+});
+
+test('Page Playwright test', async ({page})=>
+{ 
+    await page.goto("https://google.com");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
+
+});
