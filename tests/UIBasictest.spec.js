4055fa77f93118f952cfb137aab6252be099661b
aaditya mankar <aadityamankar18@gmail.com>
2026-07-02 11:17:21 +0530

    UI Updates

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -12,14 +12,15 @@ test('First Playwright test', async ({browser})=>
     const cardTitles = page.locator(".card-body a");
     const dropdown = await page.locator("select.form-control");
     const radioButton = await page.locator("span.checkmark");
+    const documentLink = await page.locator("[href*='documents-request']");
 
-    
+
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
     await userName.fill("rahulshetty");
     await password.fill("Learning");
     await signInButton.click();
-    console.log(await page.locator("[style*='block']").textContent());
+    console.log(await page.locator("[style*='block']").inputValue());
     await expect(page.locator("[style*='block']")).toContainText("Incorrect");
 
     await userName.fill("");
@@ -40,7 +41,12 @@ test('First Playwright test', async ({browser})=>
     await expect(page.locator("#terms")).toBeChecked();
     await page.locator("#terms").uncheck();
     expect(await page.locator("#terms").isChecked()).toBeFalsy();
-    await page.pause(); //- inspector for debug
+
+
+    await expect (documentLink).toHaveAttribute("class", "blinkingText");
+
+
+    //await page.pause(); //- inspector for debug
 
 
     await signInButton.click();
@@ -52,13 +58,41 @@ test('First Playwright test', async ({browser})=>
     console.log(allTitles);
 
 
-
-    
-
-    
-    
-
-
-
-
 });
+
+test.only('Child Window Handling', async ({browser})=>
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    const documentLink = await page.locator("[href*='documents-request']");
+
+
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),  //listen for any new page pending, rejected, fulfilled.
+        documentLink.click(),
+  
+    ])   // new page is opened.
+
+    const text = await newPage.locator(".red").textContent();
+    console.log(text);
+
+    const domainName = text.split("@")[1].split(" ")[0];
+    console.log(domainName);
+
+
+    await page.locator("#username").fill(domainName);
+    await page.locator("#username").textContent();
+    await page.pause();
+    console.log(await page.locator("#username").textContent());
+    
+
+    
+     
+
+
+
+
+
+})
